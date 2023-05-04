dcfa2bfe162cd10b7e593915de791e072991396c
Mike <64697794+M-Chapman@users.noreply.github.com>
2023-05-04 21:18:42 +0100

    added retrieval of song movie and weather for dates

---
+++ routes/getFacts.js
@@ -0,0 +1,136 @@
+/* eslint-disable linebreak-style */
+const express = require('express');
+const moment = require('moment');
+const axios = require('axios');
+const stations = require('../helpers/stations');
+
+const router = express.Router();
+
+const getDateFacts = async (yearStr, monthStr, dayStr) => {
+    let topSong;
+    let topMovie;
+
+    const data = {
+        dayStr,
+        monthStr,
+        yearStr,
+        getCountry: '0',
+    };
+    const { data: dateFacts } = await axios.request({
+        method: 'post',
+        url: 'https://www.mybirthdayfacts.com/MBFService.asmx/FetchDayHeadlines',
+        data,
+    });
+
+    if (Object.keys(dateFacts.d).length > 4) {
+        topSong = dateFacts.d[3].s.replace('|MBF|', ' by ');
+    } else {
+        topSong = 'no top song data available';
+    }
+
+    if (Object.keys(dateFacts.d).length > 4) {
+        topMovie = dateFacts.d[4].s;
+    } else {
+        topMovie = 'no top movie data available';
+    }
+
+    return [topSong, topMovie];
+};
+
+const getDateWeather = async (yearStr, monthStr, chosenStation) => {
+    let splitStr;
+    let tmax;
+    let tmin;
+    let ad;
+    let rain;
+    let sun;
+
+    const { data: dateWeather } = await axios.request({
+        method: 'get',
+        url: `https://www.metoffice.gov.uk/pub/data/weather/uk/climate/stationdata/${chosenStation.replace(' ', '').toLowerCase()}data.txt`,
+    });
+
+    if (monthStr.length > 1) {
+        splitStr = `${yearStr}  ${monthStr}`;
+    } else {
+        splitStr = `${yearStr}   ${monthStr}`;
+    }
+
+    const weatherFromDate = dateWeather.split(splitStr)[1];
+    if (weatherFromDate) {
+        [, tmax, tmin, ad, rain, sun] = weatherFromDate.split(/\s{1,10}/g);
+        tmax = tmax.replace(/\s/g, '');
+        tmin = tmin.replace(/\s/g, '');
+        ad = ad.replace(/\s/g, '');
+        rain = rain.replace(/\s/g, '');
+        sun = sun.replace(/\s/g, '');
+    } else {
+        tmax = 'No Max Temperature Data Available';
+        tmin = 'No Min Temperature Data Available';
+        ad = 'No Air Frost Data Available';
+        rain = 'No Amount of Rain Data Available';
+        sun = 'No Hours of Sun Data Available';
+    }
+
+    if (sun === '---') {
+        sun = 'No Hours of Sun Data Available';
+    }
+
+    return [tmax, tmin, ad, rain, sun];
+};
+
+router.get('/', async (req, res) => {
+    try {
+        const getDateData = [];
+        const years = new Date().getFullYear();
+        const {
+            year: yearStr,
+            month: monthStr,
+            day: dayStr,
+            station: chosenStation,
+        } = req.query;
+
+        // get facts on date
+        getDateData.push(getDateFacts(yearStr, monthStr, dayStr));
+        // get weather on month and year
+        getDateData.push(getDateWeather(yearStr, monthStr, chosenStation));
+
+        const [
+            // getFacts
+            [
+                topSong,
+                topMovie,
+            ],
+            // getWeather
+            [
+                tmax,
+                tmin,
+                ad,
+                rain,
+                sun,
+            ],
+        ] = await Promise.all(getDateData);
+
+        res.render('getFacts', {
+            title: 'Day Facts',
+            years,
+            yearStr,
+            months: moment.months(),
+            monthStr,
+            dayStr,
+            stations,
+            chosenStation,
+            topSong,
+            topMovie,
+            tmax,
+            tmin,
+            ad,
+            rain,
+            sun,
+        });
+    } catch (e) {
+        throw new Error(e);
+    }
+});
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,18 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+
+const router = express.Router();
 const moment = require('moment');
+const stations = require('../helpers/stations');
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  const years = new Date().getFullYear();
-  res.render('index', { title: 'Day Facts', years, months: moment.months() });
-});
-
-router.get('/getFacts/*', function (req, res) {
-  console.log("getFacts - START");
-  const years = new Date().getFullYear();
-  const { year: chosenYear, month: chosenMonth, day: chosenDay } = req.query;
-  res.render('index', { title: 'Day Facts', chosenYear, chosenMonth, chosenDay, years, months: moment.months() });
-  console.log("getFacts - END");
+router.get('/', (req, res) => {
+    const years = new Date().getFullYear();
+    res.render('index', {
+        title: 'Day Facts',
+        stations,
+        years,
+        months: moment.months(),
+    });
 });
 
 module.exports = router;
